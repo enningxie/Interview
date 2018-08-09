e3f813fc4aaa08dc8684d772461ca315ea71d5ce
enningxie <enningxie@163.com>
2018-08-10 00:23:42 +0800

    c++

---
+++ C++/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main_() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ C++/test_.cpp
@@ -0,0 +1,11 @@
+//
+// Created by enningxie on 18-8-10.
+//
+
+#include <iostream>
+using namespace std;
+
+int main_test_(){
+    cout << "xz" << endl;
+    return 0;
+}
